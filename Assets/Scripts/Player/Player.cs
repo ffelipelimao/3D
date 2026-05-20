82b74197b3736b2e69bfc91b98b77a521e61c7d4
Luiz Felipe Limao <lflimao@hotmail.com>
2026-05-19 23:22:56 -0300

    Add player movement and animation script, update project settings

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CharacterController characterController;
+    public Animator anim;
+    public float speed = 1f;
+    public float turnSpeed = 1f;
+    public float gravity = 9.8f;
+    private float vSpeed = 0f;
+    public float jumpSpeed = 15f;
+    public KeyCode keyCodeJump = KeyCode.Space;
+    [Header("Run Setup")]
+    public KeyCode keyRun = KeyCode.LeftShift;
+    public float speedRun = 1.5f;
+
+    void Update()
+    {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
+
+        var inputAxisVertical = Input.GetAxis("Vertical");
+        var speedVector = transform.forward * inputAxisVertical * speed;
+
+        if (characterController.isGrounded)
+        {
+            vSpeed = 0;
+
+            if (Input.GetKeyDown(keyCodeJump))
+            {
+                vSpeed = jumpSpeed;
+            }
+        }
+
+        vSpeed -= gravity * Time.deltaTime;
+        speedVector.y = vSpeed;
+
+        var isWalking = inputAxisVertical != 0;
+
+        if (isWalking)
+        {
+            if (Input.GetKey(keyRun))
+            {
+                speedVector *= speedRun;
+                anim.speed = speedRun;
+            }
+            else
+            {
+                anim.speed = 1;
+            }
+        }
+
+        characterController.Move(speedVector * Time.deltaTime);
+
+        anim.SetBool("Run", inputAxisVertical != 0);
+
+        /* if (inputAxisVertical != 0)
+         {
+             anim.SetBool("Run", true);
+         }
+         else
+         {
+             anim.SetBool("Run", false);
+         }
+         */
+    }
+}
