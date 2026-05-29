ccdb7315741e5a33d1f50859c422efe687a6689e
Luiz Felipe Limao <lflimao@hotmail.com>
2026-05-28 22:45:27 -0300

    fix: change gun

--- Assets/Scripts/Player/PlayerAbilityShoot.cs
+++ Assets/Scripts/Player/PlayerAbilityShoot.cs
@@ -1,26 +1,43 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerAbilityShoot : PlayerAbilityBase
 {
 
-    public GunBase gunBase;
+    public List<GunBase> guns;
     public Transform gunPosition;
     private GunBase _currentGun;
+    private int _currentGunIndex = -1;
 
     protected override void Init()
     {
         base.Init();
-        CurrentGun();
+        EquipGun(0);
         inputs.Gameplay.Shoot.performed += ctx => StartShoot();
         inputs.Gameplay.Shoot.canceled += ctx => CancelShoot();
     }
 
-    void CurrentGun()
+    void Update()
     {
-        _currentGun = Instantiate(gunBase, gunPosition);
-        _currentGun.transform.localPosition = _currentGun.transform.localEulerAngles = Vector3.zero;
+        if (Input.GetKeyDown(KeyCode.Alpha1)) EquipGun(0);
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) EquipGun(1);
+    }
+
+    void EquipGun(int index)
+    {
+        if (index < 0 || index >= guns.Count) return;
+        if (index == _currentGunIndex) return;
+
+        if (_currentGun != null)
+        {
+            _currentGun.StopShoot();
+            Destroy(_currentGun.gameObject);
+        }
+
+        _currentGun = Instantiate(guns[index], gunPosition);
+        _currentGun.transform.localPosition = Vector3.zero;
+        _currentGun.transform.localEulerAngles = Vector3.zero;
+        _currentGunIndex = index;
     }
 
 
